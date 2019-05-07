96a03d95041bece6e6c418966678a72cb8011f47
latte <yuecong97@hotmail.com>
2019-05-07 11:10:26 +0800

    create_qq and update 1369j

--- 1369j/v2/1369j_overver_v2.py
+++ 1369j/v2/1369j_overver_v2.py
@@ -9,7 +9,43 @@ import random
 import re
 import sys
 #qq部分
-from qq_send import send_qq
+import win32gui
+import win32con
+import win32clipboard as w
+import time
+
+
+def getText():
+    """获取剪贴板文本"""
+    w.OpenClipboard()
+    d = w.GetClipboardData(win32con.CF_UNICODETEXT)
+    w.CloseClipboard()
+    return d
+
+
+def setText(aString):
+    """设置剪贴板文本"""
+    w.OpenClipboard()
+    w.EmptyClipboard()
+    w.SetClipboardData(win32con.CF_UNICODETEXT, aString)
+    w.CloseClipboard()
+
+
+def send_qq(to_who, msg):
+    """发送qq消息
+    to_who：qq消息接收人
+    msg：需要发送的消息
+    """
+    # 将消息写到剪贴板
+    setText(msg)
+    # 获取qq窗口句柄
+    qq = win32gui.FindWindow(None, to_who)
+    # 投递剪贴板消息到QQ窗体
+    win32gui.SendMessage(qq, 258, 22, 2080193)
+    win32gui.SendMessage(qq, 770, 0, 0)
+    # 模拟按下回车键
+    win32gui.SendMessage(qq, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
+    win32gui.SendMessage(qq, win32con.WM_KEYUP, win32con.VK_RETURN, 0)
 
 def my_conn(host,user,passwd,port,db):
     conn = pymysql.connect(
@@ -167,6 +203,6 @@ if __name__ == '__main__':
                 time.sleep(60)
             my_close(conn=conn)
         except Exception as e:
-            print(e,'')
-
+            print(e,'出现错误停顿2-3秒后重新运行')
+            time.sleep(random.uniform(2,3))
 

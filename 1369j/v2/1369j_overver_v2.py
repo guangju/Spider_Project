23129f975e4fbaba01fe6e3934bec6d98021a25b
latte <yuecong97@hotmail.com>
2019-05-07 11:06:42 +0800

    update

--- 1369j/v2/1369j_overver_v2.py
+++ 1369j/v2/1369j_overver_v2.py
@@ -9,45 +9,7 @@ import random
 import re
 import sys
 #qq部分
-
-
-import win32gui
-import win32con
-import win32clipboard as w
-import time
-
-
-def getText():
-    """获取剪贴板文本"""
-    w.OpenClipboard()
-    d = w.GetClipboardData(win32con.CF_UNICODETEXT)
-    w.CloseClipboard()
-    return d
-
-
-def setText(aString):
-    """设置剪贴板文本"""
-    w.OpenClipboard()
-    w.EmptyClipboard()
-    w.SetClipboardData(win32con.CF_UNICODETEXT, aString)
-    w.CloseClipboard()
-
-
-def send_qq(to_who, msg):
-    """发送qq消息
-    to_who：qq消息接收人
-    msg：需要发送的消息
-    """
-    # 将消息写到剪贴板
-    setText(msg)
-    # 获取qq窗口句柄
-    qq = win32gui.FindWindow(None, to_who)
-    # 投递剪贴板消息到QQ窗体
-    win32gui.SendMessage(qq, 258, 22, 2080193)
-    win32gui.SendMessage(qq, 770, 0, 0)
-    # 模拟按下回车键
-    win32gui.SendMessage(qq, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
-    win32gui.SendMessage(qq, win32con.WM_KEYUP, win32con.VK_RETURN, 0)
+from qq_send import send_qq
 
 def my_conn(host,user,passwd,port,db):
     conn = pymysql.connect(
@@ -163,6 +125,7 @@ if __name__ == '__main__':
 
 
     while True:
+        try:
             conn = my_conn(host='www.muming8.com', user='zhang', passwd='zhang.123', port=3306, db='kehu')
             old_data = my_select(conn=conn)
             url = 'https://www.1396j.com/xyft/kaijiang'
@@ -203,5 +166,7 @@ if __name__ == '__main__':
                 print('未检测到新数据更新，等待一分钟')
                 time.sleep(60)
             my_close(conn=conn)
+        except Exception as e:
+            print(e,'')
 
 
